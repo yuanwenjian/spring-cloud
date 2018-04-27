014c4404ddb3acae30dbc21dd66c26f45006ba68
xuziling <xuziling@centaur.cn>
2018-04-27 19:03:32 +0800

    提交sleuth模块

--- config-client/src/main/java/com/yuanwj/rest/TestResource.java
+++ config-client/src/main/java/com/yuanwj/rest/TestResource.java
@@ -1,6 +1,8 @@
 package com.yuanwj.rest;
 
 import com.yuanwj.config.BiocloudProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,14 +17,16 @@ import javax.inject.Inject;
 @RequestMapping(value = "/api/v1/test/")
 public class TestResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestResource.class);
+
     @Inject
     private  BiocloudProperties biocloudProperties;
 
     @RequestMapping(value = "test",method = RequestMethod.GET)
     public String test(){
-        System.out.println(biocloudProperties.getFtp().getHost());
-        System.out.println(biocloudProperties.getFtp().getPort());
-        System.out.println(biocloudProperties.getFtp().getUser());
+        LOG.debug(biocloudProperties.getFtp().getHost());
+        LOG.debug(biocloudProperties.getFtp().getPort()+"");
+        LOG.debug(biocloudProperties.getFtp().getUser());
         return "success";
     }
 
--- gateway/src/main/java/com/yuanwj/GatewayApplication.java
+++ gateway/src/main/java/com/yuanwj/GatewayApplication.java
@@ -1,18 +1,46 @@
 package com.yuanwj;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        LOG.debug("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
+    }
 }
 
 
